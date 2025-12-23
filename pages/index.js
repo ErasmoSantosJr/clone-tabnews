ebc303108f14f9f1f3d3f74a969325b2ea84f703
Erasmo Santos <50470929+ErasmoSantosJr@users.noreply.github.com>
2025-12-23 15:23:27 +0000

    adicionado a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home(){
+    return (
+        <>
+            <span>“Dor, ressentimento, tristeza, mágoa e desespero…</span>
+            <br></br>
+            <span>Tenho certeza que também lhe trará miséria às vezes.</span>
+            <br></br>
+            <span>
+                E tenho certeza de que você também terá força para passar por todas essas provações. 
+                Não é isso o amor?”
+            </span>
+            <br></br>
+            <span><i>- It's Okay That's Love.</i></span>
+        </>
+    )
+}
+
+export default Home;
